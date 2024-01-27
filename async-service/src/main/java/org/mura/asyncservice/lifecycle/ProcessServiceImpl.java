8ac25f1734f94514cc43ff698a17ae6030dc4ecf
mura <muramega9@gmail.com>
2024-01-28 02:30:20 +0600

    update add timeout

--- async-service/src/main/java/org/mura/asyncservice/lifecycle/ProcessServiceImpl.java
+++ async-service/src/main/java/org/mura/asyncservice/lifecycle/ProcessServiceImpl.java
@@ -8,6 +8,7 @@ import org.mura.asyncservice.service.ModelService;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -16,6 +17,8 @@ import java.util.concurrent.TimeUnit;
 public class ProcessServiceImpl implements ProcessService {
 
     private final ModelService modelService;
+
+    private final Executor executor;
 
     @Override
     public void start(ModelDTO modelDTO) {
@@ -59,7 +62,6 @@ public class ProcessServiceImpl implements ProcessService {
             start.get();
             continues.get();
 
-
         } catch (Exception e) {
             log.error(e.getMessage());
         }
@@ -67,7 +69,6 @@ public class ProcessServiceImpl implements ProcessService {
 
     private void resultByJoin() {
         try {
-
 
             CompletableFuture<Void> start = CompletableFuture
                     .runAsync(modelService::doSave).orTimeout(1000, TimeUnit.MILLISECONDS);
@@ -78,9 +79,43 @@ public class ProcessServiceImpl implements ProcessService {
 
             CompletableFuture.allOf(start, continues).join();
 
-
         } catch (Exception e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void asyncMethod() {
+        try {
+
+            CompletableFuture<String> asyncStartWithExecutor = CompletableFuture
+                    .supplyAsync(() -> "1", executor);
+
+            CompletableFuture<String> asyncStart = CompletableFuture
+                    .supplyAsync(() -> "1");
+
+            CompletableFuture<String> complete = new CompletableFuture<>();
+
+            simulateLongRunningTask(complete);
+
+            complete.completeOnTimeout("TimeoutValue", 1, TimeUnit.MILLISECONDS);
+
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void simulateLongRunningTask(CompletableFuture<String> future) {
+        // Simulate a long-running task that takes more than 1 second
+        new Thread(() -> {
+            try {
+                // Simulating a task that takes 5 seconds
+                Thread.sleep(5000);
+                future.complete("Task Result");
+            } catch (InterruptedException e) {
+                // Handle interruption if needed
+                future.completeExceptionally(e);
+            }
+        }).start();
     }
 }
